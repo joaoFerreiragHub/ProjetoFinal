19c7875bda80f345d4f03abb273b0bda90a35881
joaoFerreiragHub <96747056+joaoFerreiragHub@users.noreply.github.com>
2022-03-24 16:25:50 +0000

    8.2ImportCSVFailed

---
+++ ProjetoFoodTracker/ProjetoFoodTracker/Pages/ImportCSV.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjetoFoodTracker.Pages
+{
+    public class ImportCSVModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
