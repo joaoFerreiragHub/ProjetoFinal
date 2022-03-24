80ed06239dc4371b9385c3dcdc6a58b38603156d
joaoFerreiragHub <96747056+joaoFerreiragHub@users.noreply.github.com>
2022-03-24 13:49:18 +0000

    7.1_Compositekey_Column_order

--- ProjetoFoodTracker/ProjetoFoodTracker/Data/Entities/FoodAction.cs
+++ ProjetoFoodTracker/ProjetoFoodTracker/Data/Entities/FoodAction.cs
@@ -1,8 +1,13 @@
-﻿namespace ProjetoFoodTracker.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoFoodTracker.Data.Entities
 {
     public class FoodAction
     {
+        [Column(Order = 0)]
         public int FoodId { get; set; }
+        [Column(Order = 1)]
         public int ActionId { get; set; }
         public Food Food { get; set; }
         public Actions Actions { get; set; }
--- ProjetoFoodTracker/ProjetoFoodTracker/Data/Entities/FoodCategory.cs
+++ ProjetoFoodTracker/ProjetoFoodTracker/Data/Entities/FoodCategory.cs
@@ -1,8 +1,12 @@
-﻿namespace ProjetoFoodTracker.Data.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoFoodTracker.Data.Entities
 {
     public class FoodCategory
     {
+        [Column(Order = 0)]
         public int FoodId { get; set; }
+        [Column(Order = 1)]
         public int CategoryId { get; set; }
         public Food Food { get; set; }
         public Category Category { get; set; }
